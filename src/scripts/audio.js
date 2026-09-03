b0c8b5ebe1b40e0141c333083a4012d603cc2d3b
Đat <datnguyen.github@gmail.com>
2026-09-03 23:58:21 +0700

    d

--- src/scripts/audio.js
+++ src/scripts/audio.js
@@ -28,7 +28,7 @@ function playRandomSong() {
             .play()
             .then(() => {
                 isMusicPlaying = true;
-                if (musicBtn) musicBtn.textContent = '🎵git s';
+                if (musicBtn) musicBtn.textContent = '🎵';
             })
             .catch((e) => console.log('Trình duyệt chặn tự động phát:', e));
     }, 500);
